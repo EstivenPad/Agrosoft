b730bba4fe63777a036dfac833e97e38e869cf49
Estiven Padilla <estivenps26@gmail.com>
2020-06-24 14:53:55 -0400

    Configurando el Blazored Toast

--- Agrosoft/Startup.cs
+++ Agrosoft/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Agrosoft.Data;
+using Blazored.Toast;
 
 namespace Agrosoft
 {
@@ -29,6 +30,7 @@ namespace Agrosoft
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddBlazoredToast();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
