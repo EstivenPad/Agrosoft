e09226aa1ac9d3f03eb70b1bf5fadc927456ec09
Estiven Padilla <estivenps26@gmail.com>
2020-06-24 13:33:28 -0400

    Creando el contexto

---
+++ Agrosoft/DAL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Agrosoft.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Agrosoft.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Usuarios> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = DATA\Agrosoft.db");
+        }
+    }
+}
