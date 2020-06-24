c6b96e78aea0b5b1d308c83b557ade06c22054b7
Estiven Padilla <estivenps26@gmail.com>
2020-06-24 13:37:26 -0400

    Generando la base de datos

---
+++ Agrosoft/Migrations/20200624173624_Migracion_Inicial.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Agrosoft.Migrations
+{
+    public partial class Migracion_Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    UsuarioId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Fecha = table.Column<DateTime>(nullable: false),
+                    Nombres = table.Column<string>(nullable: false),
+                    Apellidos = table.Column<string>(nullable: false),
+                    Telefono = table.Column<string>(nullable: false),
+                    Celular = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    Direccion = table.Column<string>(nullable: false),
+                    NombreUsuario = table.Column<string>(nullable: false),
+                    ClaveUsuario = table.Column<string>(nullable: false),
+                    TipoUsuario = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.UsuarioId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
