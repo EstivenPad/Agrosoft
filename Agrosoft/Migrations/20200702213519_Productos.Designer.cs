c09af5f7145ee0bba31af76e613dc923f6b7c936
Jose Armando Flores Baldera <jose_flores3@ucne.edu.do>
2020-07-02 17:36:57 -0400

    Creando migracion de la tabla productos

--- Agrosoft/Migrations/20200702213519_Productos.Designer.cs
+++ Agrosoft/Migrations/20200702213519_Productos.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Agrosoft.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Agrosoft.Migrations
 {
     [DbContext(typeof(Contexto))]
-    partial class ContextoModelSnapshot : ModelSnapshot
+    [Migration("20200702213519_Productos")]
+    partial class Productos
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Agrosoft/Migrations/20200702213519_Productos.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Agrosoft.Migrations
+{
+    public partial class Productos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    ProductoId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nombre = table.Column<string>(nullable: true),
+                    Marca = table.Column<string>(nullable: true),
+                    UnidadMedida = table.Column<int>(nullable: false),
+                    CantidadMinima = table.Column<int>(nullable: false),
+                    CantidadExistente = table.Column<int>(nullable: false),
+                    Precio = table.Column<decimal>(nullable: false),
+                    Costo = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.ProductoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Productos");
+        }
+    }
+}
