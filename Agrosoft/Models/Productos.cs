4fe6e19c9c26eee63555377e581a4a512c63f5db
Jose Armando Flores Baldera <jose_flores3@ucne.edu.do>
2020-07-02 17:52:24 -0400

    Actualizando base de datos

--- Agrosoft/Models/Productos.cs
+++ Agrosoft/Models/Productos.cs
@@ -10,12 +10,21 @@ namespace Agrosoft.Models
     {
         [Key]
         public int ProductoId { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir el nombre")]
         public string Nombre { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir la marca")]
         public string Marca { get; set; }
+        [Required(ErrorMessage = "Es obligatorio seleccionar la unidad de medida")]
         public int UnidadMedida { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir la cantidad minima")]
         public int CantidadMinima { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir la cantidad existente")]
         public int CantidadExistente { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir el precio")]
+        [Range(minimum: 1, maximum: double.MaxValue, ErrorMessage = "El precio debe ser mayor a 1")]
         public decimal Precio { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir el costo")]
+        [Range(minimum: 1, maximum: double.MaxValue, ErrorMessage = "El costo debe ser mayor a 1")]
         public decimal Costo { get; set; }
 
         public Productos()
