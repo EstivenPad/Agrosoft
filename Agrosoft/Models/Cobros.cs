2affd2d49a38d1f80a551bbf307d428fba27751b
Jose Armando Flores Baldera <jose_flores3@ucne.edu.do>
2020-07-14 15:29:44 -0400

    Creando entidad cobros

---
+++ Agrosoft/Models/Cobros.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Agrosoft.Models
+{
+    public class Cobros
+    {
+        [Key]
+        public int CobrosId { get; set; }
+        public DateTime Fecha { get; set; }
+        [Range(minimum: 1, maximum: double.MaxValue, ErrorMessage = "Debe seleccionar un cliente")]
+        [Required(ErrorMessage = "Debe seleccionar un cliente")]
+        public int ClienteId { get; set; }
+        public decimal Deposito { get; set; }
+        public decimal Balance { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir el límite de crédito")]
+        public decimal LimiteCredito { get; set; }
+        public int UsuarioId { get; set; }
+
+    }
+}
