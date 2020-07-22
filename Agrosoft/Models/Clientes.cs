8ba9ffe9834385fb629f3615506a495abc736d0b
Jose Armando Flores Baldera <jose_flores3@ucne.edu.do>
2020-07-22 14:34:39 -0400

    Modificando entidad clientes y registro clientes

--- Agrosoft/Models/Clientes.cs
+++ Agrosoft/Models/Clientes.cs
@@ -44,5 +44,12 @@ namespace Agrosoft.Models
 
         [Required(ErrorMessage = "Es obligatorio introducir la dirección")]
         public string Direccion { get; set; }
+
+        [Range(minimum: 1, maximum: double.MaxValue, ErrorMessage = "El límite de crédito debe ser mayor que 1")]
+        [Required(ErrorMessage = "Es obligatorio introducir el límite de crédito")]
+        public decimal LimiteCredito { get; set; }
+
+        public decimal Balance { get; set; }
+        
     }
 }
