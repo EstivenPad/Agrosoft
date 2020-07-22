457bda4d4e807e3aed3e6b4f362b697900c4d901
Jose Armando Flores Baldera <jose_flores3@ucne.edu.do>
2020-07-22 14:34:57 -0400

    Modificando entidad cobros

--- Agrosoft/Models/Cobros.cs
+++ Agrosoft/Models/Cobros.cs
@@ -16,10 +16,6 @@ namespace Agrosoft.Models
         public int ClienteId { get; set; }
         [Range(minimum: 0, maximum: double.MaxValue, ErrorMessage = "El depósito no puede ser menor que 0")]
         public decimal Deposito { get; set; }
-        public decimal Balance { get; set; }
-        [Range(minimum: 1, maximum: double.MaxValue, ErrorMessage = "El límite de crédito debe ser mayor que 1")]
-        [Required(ErrorMessage = "Es obligatorio introducir el límite de crédito")]
-        public decimal LimiteCredito { get; set; }
         public int UsuarioId { get; set; }
 
     }
